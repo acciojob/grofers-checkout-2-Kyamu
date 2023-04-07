786bb14254c276b8e2fccd58fb16660b54b9d0f4
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-04-07 07:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+// Get all the elements that store the prices
+const pricesElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Initialize the total price to 0
+let totalPrice = 0;
+
+// Loop through each prices element and add its value to the total price
+for (let i = 0; i < pricesElements.length; i++) {
+  totalPrice += parseFloat(pricesElements[i].textContent);
+}
+
+// Create the last row of the table to display the total price
+const table = document.querySelector('table');
+const row = table.insertRow();
+const cell = row.insertCell();
+cell.colSpan = 2;
+cell.textContent = `Total: $${totalPrice.toFixed(2)}`;
+
+// Set the data-ns-test attribute for the total cell
+cell.setAttribute('data-ns-test', 'grandTotal');
